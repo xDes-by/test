b5b700ce9f60bfe3f7c00a71655222dfa06cf8de
Viktor Mekka <italyevil@mail.ru>
2023-09-27 12:45:49 +0300

    dz

--- test2.py
+++ test2.py
@@ -1,1 +1,43 @@
-sss
+# num1 = int(input("Введите первое число: "))
+# sing = input("Операция: ")
+# num2 = int(input("Введите второе число: "))
+#
+# if sing == "+":
+#     print(num1+num2)
+# elif sing == "-":
+#     print(num1-num2)
+
+# expression = input("Введите выражение (например, '5+9' или '5/9' или '5*9'): ")
+#
+# if '+' in expression:
+#     operator = '+'
+# elif '-' in expression:
+#     operator = '-'
+# elif '*' in expression:
+#     operator = '*'
+# elif '/' in expression:
+#     operator = '/'
+# else:
+#     print("Неверный формат выражения")
+#     exit()
+#
+# operands = expression.split(operator)
+# print(operands)
+#
+# operand1 = float(operands[0])
+# operand2 = float(operands[1])
+#
+# if operator == '+':
+#     result = operand1 + operand2
+# elif operator == '-':
+#     result = operand1 - operand2
+# elif operator == '*':
+#     result = operand1 * operand2
+# elif operator == '/':
+#     if operand2 == 0:
+#         print("Деление на ноль недопустимо")
+#         exit()
+#     result = operand1 / operand2
+#
+# print("Результат:", result)
+
