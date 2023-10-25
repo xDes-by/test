8b9a620301438bb59202b1532774e9f0c58b8ab4
Viktor Mekka <italyevil@mail.ru>
2023-10-25 15:03:47 +0300

    D

--- dsfjdsh.py
+++ dsfjdsh.py
@@ -9,3 +9,17 @@
 #     index = (index + k) % len(elements)
 #
 # print("Последний оставшийся элемент:", elements[0])
+shopping_list=[]
+shopping_list.append("яблоко")
+shopping_list.append("молоко")
+shopping_list.append("молоко")
+shopping_list.append("хлеб")
+shopping_list.append("яйца")
+shopping_list.append("сок")
+for d in shopping_list:
+    print(d)
+shopping_list.remove("молоко")
+shopping_list[shopping_list.index("яблоко")]= 'банан'
+print(shopping_list)
+print(len(shopping_list))
+input()
